e9285d95015d0a391b072d79519b127acbb56fe5
athifal <101647187+athifal@users.noreply.github.com>
2024-07-12 19:29:28 +0530

    Update hexagon.py

--- hexagon.py
+++ hexagon.py
@@ -1,11 +1,14 @@
-
-
 def print_hexagon_pattern(rows, cols):
     top_line = " ___    "
     mid_top_line = "/   \\___"
     mid_bottom_line = "\\___/   "
     last_line = "\\___/   "
     end_pattern = "/   \\"
+    lastw="/   \\___/"
+    lasty="/   \\___"
+    iot="\\"
+    cot="___/"
+    got="\\___/"
     if cols % 2 == 0:
       a = cols // 2
     else:
@@ -17,15 +20,26 @@ def print_hexagon_pattern(rows, cols):
            print()
         else:
            for col in range(a):
+                
+                
                 print(last_line, end='')
+                if (col==a-1) and (row!=rows-1) and (cols%2==0):
+                 print(iot,end='')
            print()
 
-        if row==rows-1:
+        if (row==rows-1)and(rows>1):
           break
 
         for col in range(a):
-            if col == a - 1:
-                print(end_pattern, end='')
+            if (col == a - 1):
+              if (cols%2==0) :
+                if row!=0:
+                 print(lastw, end='')
+                else:
+                 print(lasty,end='')
+
+              else:
+                print(end_pattern,end='')
             else:
                 print(mid_top_line, end='')
         print()
@@ -39,14 +53,37 @@ def print_hexagon_pattern(rows, cols):
 
         for col in range(a):
                 print(mid_bottom_line, end='')
+                if col ==a-1:
+                  if cols%2==0:
+                   print(iot,end='')
+                  
+                    
+                
+                
         print()
-        if row < rows - 1:
+        if row < rows-1:
             for col in range(a):
                 if col == a - 1:
                     print(end_pattern, end='')
+                    if cols%2==0:
+  
+                     print(cot,end='')
                 else:
                     print(mid_top_line, end='')
             print()
+        if rows==1:
+          if cols%2==0:
+            a=a
+          else:
+            a=a-1;
+          for col in range(a):
+              if col == 0:
+               print("    \\___/", end='   ')  # Print the initial string with spaces
+              else:
+               print(got, end='   ')  # Ensure got has no leading spaces
+          print()
+            
+
 rows = int(input("Enter the number of rows: "))
 cols = int(input("Enter the number of columns: "))
-print_hexagon_pattern(rows, cols)
+print_hexagon_pattern(rows,cols)
